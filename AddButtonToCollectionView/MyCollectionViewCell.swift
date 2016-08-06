c3542d369202526f6636b11ebeaa75ff1c4db7a9
Scott Duffy <duffyscottc@gmail.com>
2016-08-06 11:31:28 -0400

    I was successful in adding a button to each cell.

---
+++ AddButtonToCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  AddButtonToCollectionView
+//
+//  Created by Scott on 8/6/16.
+//  Copyright © 2016 Brosnappedfink. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
--- AddButtonToCollectionView/ViewController.swift
+++ AddButtonToCollectionView/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBAction func pressButton(_ sender: AnyObject) { //The button inside a cell
+        
+        print("Hello World")
+        
+    }
+    
+    var myArray = ["", "", "", "", "", ""]
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: MyCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MyCollectionViewCell
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
